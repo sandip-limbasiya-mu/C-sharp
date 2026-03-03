9c92bed7cbe8a5f9fdf92331004bdebc28e32f89
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-03 08:10:56 +0530

    3/3/2026

---
+++ Program_12/Program_12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        int a = 10;
+        float b = 5.5f;
+
+        float result1;
+        int result2;
+
+        result1 = a + b;
+
+        result2 = a + (int)b;
+
+
+        Console.WriteLine("Integer value: " + a);
+        Console.WriteLine("Float value: " + b);
+
+        Console.WriteLine("\nAfter Automatic Conversion (a + b): " + result1);
+        Console.WriteLine("After Force Conversion (a + (int)b): " + result2);
+    }
+}
---
+++ Program_3/Program_3/Program.cs
@@ -0,0 +1,27 @@
+﻿//w.a.p to input 2 number and an arithmetic operator , display the result 
+using System;
+
+namespace App
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.Write("Enter no 1 : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter no 2 : ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Addition: " + (a + b));
+            Console.WriteLine("Subtraction: " + (a - b));
+            Console.WriteLine("Multiplication: " + (a * b));
+            Console.WriteLine("Division: " + (a / b));
+            Console.WriteLine("Modulus: " + (a % b));
+
+        }
+    }
+}
