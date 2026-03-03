b19b7b88b7c210fa06485ca9ce744069fb65c8ef
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-03 08:25:44 +0530

    03-03-26

---
+++ Program_25/Program_25/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int max = Int32.MinValue;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write("Enter number " + i + ": ");
+            int currentNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (currentNumber > max)
+            {
+                max = currentNumber;
+            }
+        }
+
+        Console.WriteLine("Maximum number is: " + max);
+    }
+}
