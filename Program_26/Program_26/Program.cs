9c188914397031a974d43d03a2272d2a0c789926
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-03 08:28:01 +0530

    03-03-26

---
+++ Program_26/Program_26/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int min = Int32.MaxValue;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write("Enter number " + i + ": ");
+            int currentNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (currentNumber < min)
+            {
+                min = currentNumber;
+            }
+        }
+
+        Console.WriteLine("Minimum number is: " + min);
+    }
+}
