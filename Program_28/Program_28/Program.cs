6f27a07371598ecd3d84740fe6d2fa6ccc3319dc
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-13 17:25:33 +0530

    update code

---
+++ Program_28/Program_28/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] marks = new int[5];
+        int sum = 0;
+        double average;
+
+        Console.WriteLine("Enter marks of 5 students:");
+        for (int i = 0; i < 5; i++)
+        {
+            marks[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            sum = sum + marks[i];
+        }
+        average = sum / 5.0;
+
+        Console.WriteLine("Total Marks = " + sum);
+        Console.WriteLine("Average Marks = " + average);
+    }
+}
