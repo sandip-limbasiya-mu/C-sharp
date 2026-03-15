57ef2c944b06dd6923b73235004f4880cca70533
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-15 22:47:59 +0530

    3/3/26

---
+++ Program_4/Program_4/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int p, r, t, si;
+
+        Console.Write("Enter Principal: ");
+        p = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Rate: ");
+        r = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Time: ");
+        t = int.Parse(Console.ReadLine());
+
+        si = (p * r * t) / 100;
+
+        Console.WriteLine("Simple Interest = " + si);
+    }
+}
