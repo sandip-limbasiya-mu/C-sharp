e6c706d203794990489dc4ce6e6f2886571f3978
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-15 22:54:29 +0530

    3/3/2026

---
+++ Program_9/Program_9/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace EvenOddChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check:");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + " is an Even number.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is an Odd number.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
