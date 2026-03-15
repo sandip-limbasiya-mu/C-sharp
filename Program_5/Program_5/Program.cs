78b1c6f74b6f216303c3295d01b71f5f28ff6118
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-15 22:50:20 +0530

    3/3/2026

---
+++ Program_5/Program_5/Program.cs
@@ -0,0 +1,22 @@
+﻿//input from user perform all relational operator and display correct output
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b;
+
+        Console.Write("Enter Value 1 : ");
+        a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Value 2 : ");
+        b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("A Greater than B : " + (a > b));
+        Console.WriteLine("A Less than B : " + (a < b));
+        Console.WriteLine("A Greater than equal to B : " + (a >= b));
+        Console.WriteLine("A Less than equal to B : " + (a <= b));
+        Console.WriteLine("A not equal to B : " + (a != b));
+    }
+}
