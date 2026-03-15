728061b2e5b7325a4c39c1d18493a468a5a9542a
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-15 22:53:49 +0530

    3/3/2026

---
+++ Program_8/Program_8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle:");
+            string input = Console.ReadLine();
+            double radius = Convert.ToDouble(input);
+
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine("The Area of the circle is: " + area);
+
+            Console.ReadKey();
+        }
+    }
+}
