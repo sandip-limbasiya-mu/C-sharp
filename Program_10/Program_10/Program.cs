a06face10898975e120e8f01f2950bebdcccde4e
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-15 22:55:01 +0530

    3/3/2026

---
+++ Program_10/Program_10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AgeCategorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            string input = Console.ReadLine();
+            int age = Convert.ToInt32(input);
+
+            if (age < 12)
+            {
+                Console.WriteLine("You are a Kid");
+            }
+            else if (age <= 17)
+            {
+                Console.WriteLine("You are a Teenager");
+            }
+            else if (age <= 60)
+            {
+                Console.WriteLine("You are an Adult");
+            }
+            else
+            {
+                Console.WriteLine("You are a Senior Citizen");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
