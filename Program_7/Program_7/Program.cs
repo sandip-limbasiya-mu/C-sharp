eb6f702cef8d2ec0584ac1385ff63fc79e8c0782
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-15 22:52:45 +0530

    3/3/2026

---
+++ Program_7/Program_7/Program.cs
@@ -0,0 +1,24 @@
+﻿// w.a.p to input principal amount , rate and year and display compound interest 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double p, r, y, ci;
+
+        Console.Write("Enter Principal Amount: ");
+        p = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Rate of Interest: ");
+        r = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Time (in years): ");
+        y = double.Parse(Console.ReadLine());
+
+        ci = p * Math.Pow((1 + r / 100), y) - p;
+
+        Console.WriteLine("Compound Interest : " + ci);
+    }
+}
