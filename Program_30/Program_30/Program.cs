226376ace91b695c1c15166fb3a7c14a3ab2f201
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-17 08:06:34 +0530

    17/3/2026

---
+++ Program_30/Program_30/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Student
+{
+    int rollNo;
+    string name;
+    double marks;
+
+    public Student()
+    {
+        rollNo = 0;
+        name = "raj";
+        marks = 0;
+    }
+
+    public Student(int r, string n, double m)
+    {
+        rollNo = r;
+        name = n;
+        marks = m;
+    }
+
+    public string CalculateGrade()
+    {
+        if (marks >= 90)
+            return "A";
+        else if (marks >= 75)
+            return "B";
+        else if (marks >= 60)
+            return "C";
+        else if (marks >= 50)
+            return "D";
+        else
+            return "F";
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Roll No: " + rollNo);
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Marks: " + marks);
+        Console.WriteLine("Grade: " + CalculateGrade());
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student(101, "jay", 82);
+
+        Student s2 = new Student();
+
+        Console.WriteLine("Student 1 Details:");
+        s1.DisplayDetails();
+
+        Console.WriteLine("\nStudent 2 Details:");
+        s2.DisplayDetails();
+    }
+}
