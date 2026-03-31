c0c4f6ceca1c2fee90a8099ff05f9e802e515bd0
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-03-31 07:59:16 +0530

    24/3/2026

---
+++ Program_33/Program_33/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+interface Exam
+{
+    bool Pass(int mark);
+}
+
+interface Classify
+{
+    string Division(int average);
+}
+
+class Result : Exam, Classify
+{
+
+    public bool Pass(int mark)
+    {
+        return mark >= 50;
+    }
+
+    public string Division(int average)
+    {
+        if (average >= 60)
+            return "First";
+        else if (average >= 50)
+            return "Second";
+        else
+            return "No division";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Result r = new Result();
+
+        int mark = 55;
+        int average = 58;
+
+        if (r.Pass(mark))
+            Console.WriteLine("Result: Pass");
+        else
+            Console.WriteLine("Result: Fail");
+
+        Console.WriteLine("Division: " + r.Division(average));
+    }
+}
