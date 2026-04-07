65903cffb2e5aaac052be77ea550610a3f73e063
sandip-limbasiya-mu <mugithub01@gmail.com>
2026-04-07 08:00:55 +0530

    07/4/26

---
+++ Program_36/Program_36/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int result = 100 / num;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: Division by zero is not allowed.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Invalid input format.");
+        }
+        finally
+        {
+            Console.WriteLine("This block always executes.");
+        }
+
+        Console.WriteLine("End of program.");
+    }
+}
